43add1079f688ac55273bb909fa8fd95a1065f3b
Max Bacon <maxbac11@gmail.com>
2023-02-26 22:19:56 -0600

    Revert "Rename"
    This reverts commit 95e4cad055fc5dd0a322517941381dac5fa20813.

--- main.py
+++ main.py
@@ -2,27 +2,39 @@
 
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfile
-import request
-import atom
-import contact
-import excel
-import read
+from openpyxl import Workbook
+import requests
 
-
+wb = Workbook()
+ws = wb.active
 
 url = "https://swift.cmbi.umcn.nl/cgi/GenericCGI.py"
 
 # Ask
 
 minDist = float(input("Please enter the minimum HBond distance: "))
-minAngle = float(input("Please enter the minimum HBond angle: "))
+
+print("Please select the desired save location:")
+save_file = asksaveasfile(initialfile = 'Untitled.xlsx', defaultextension=".xlsx", filetypes=[("Excel","*.xlsx*")]).name
 
 print("Please select the PDB file:")
 pdb_file = filedialog.askopenfilename()
 
-print("Please select the desired save location:")
-save_file = asksaveasfile(initialfile = "{loc}_Output.xlsx".format(loc = pdb_file[pdb_file.rindex("/") + 1:pdb_file.rindex(".pdb")]), defaultextension=".xlsx", filetypes=[("Excel","*.xlsx*")]).name
-atoms = read.makeAtomsFromPDB(open(pdb_file,"r"))
+# Request
+
+payload={'request': 'allhbonds',
+'&PDB1': '',
+'SubmitButton': 'Send'}
+files=[
+  ('&FIL1',('5N4L_1101.pdb',open(pdb_file,'rb'),'application/octet-stream'))
+]
+headers = {}
+
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+response = response.text[response.text.find("?"):]
+response = response[0:response.find("\"")]
+
+finalResponse = requests.get(url + response)
 
 # Interpret
 
@@ -33,15 +45,19 @@ def isHead(string):
         return False
 
 
-data = request.requestServer(pdb_file).split("\n")
+data = finalResponse.text.split("\n")
 del data[0:8], data[len(data) - 1]
+
+ws.append(["Ligand (Source...Target)", "Bond Distance"])
+
+export = []
 
 nextExp = []
 for i in range(len(data)):
     
     if isHead(data[i]):
         if len(nextExp) > 1:
-            excel.addExport(nextExp.copy())
+            export.append(nextExp.copy())
             nextExp.clear()
         elif len(nextExp) > 0:
             nextExp.clear()
@@ -57,15 +73,12 @@ for i in range(len(data)):
             hasR = split[n].find("\r")
             if hasR > -1:
                 split[n] = split[n].replace("\r", "")
-        print(split)
-
-        read.generateDataFromPDB(data[i])
 
         out = "{0} {1} ({2}) {4}...{6} {7} ({8}) {10}".format(*split)
         nextExp.append(out)
     else:
         if len(nextExp) > 1:
-            excel.addExport(nextExp.copy())
+            export.append(nextExp.copy())
             nextExp.clear()
             nextExp.append("")
         
@@ -89,6 +102,8 @@ for i in range(len(data)):
         nextExp.append(split[3])
 
 
-excel.save(save_file)
+[ws.append(x) for x in export]
+
+wb.save(save_file)
 
 print(f"Formatted HBonds have been saved to {save_file}")
